7084a06be1ac277b1f38fe75929b6be41f054b07
Dima <metiq2007@yandex.ru>
2025-04-11 00:26:09 +0300

    Update config.py

--- user/core/config.py
+++ user/core/config.py
@@ -1,21 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from motor.motor_asyncio import AsyncIOMotorClient
 
-class Settings(BaseSettings):
-    #MongoSettings
-    MONGO_INITDB_ROOT_USERNAME: str
-    MONGO_INITDB_ROOT_PASSWORD: str
-    MONGO_INITDB_DATABASE: str
-
-    ME_CONFIG_BASICAUTH_USERNAME: str
-    ME_CONFIG_BASICAUTH_PASSWORD: int
-    ME_CONFIG_MONGODB_SERVER: str
-    ME_CONFIG_MONGODB_HOST: str
-    ME_CONFIG_MONGODB_PORT: int
-    ME_CONFIG_MONGODB_ADMINUSERNAME: str
-    ME_CONFIG_MONGODB_ADMINPASSWORD: str
-    ME_CONFIG_MONGODB_ENABLE_ADMIN: bool
-    
+class Settings(BaseSettings):    
     #PostgreSQL
     POSTGRESQL_HOST: str
     POSTGRESQL_PORT: int
@@ -32,11 +18,6 @@ class Settings(BaseSettings):
     #Redis settings
     REDIS_HOST: str
     REDIS_PORT: int
-    
-    @property
-    def MONGODB_CLIENT(self):
-        return AsyncIOMotorClient(f'mongodb://{self.MONGO_INITDB_ROOT_USERNAME}:{self.MONGO_INITDB_ROOT_PASSWORD}@'
-                f'{self.ME_CONFIG_MONGODB_HOST}:{self.ME_CONFIG_MONGODB_PORT}/{self.MONGO_INITDB_DATABASE}')
     
     @property
     def POSTGRES_URL(self):
