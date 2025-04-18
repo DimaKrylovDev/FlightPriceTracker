ba37764059899a8390ecb2f2f9a5f46ef65f13fd
DimaKrylov <tumbo4kadima@yandex.ru>
2025-04-19 02:12:18 +0300

    added some methods

---
+++ trips/core/utils.py
@@ -0,0 +1,28 @@
+import json
+from core.config import settings
+from db.models.trips import Trips
+from db.models.user_town import UserTown
+from fastapi import HTTPException, status
+
+
+def city_code(town: str):
+    with open(settings.CITIES_JSON_PATH) as json_cities:
+        json_cities = json.load(json_cities)
+        
+    for cities in json_cities:
+        if cities['name'] == town:
+            return cities['code']   
+        
+async def check_trip(trip_number: int):
+    res = await Trips.find_one({'trip_number': trip_number})
+    if res:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="This trip already exist")
+    
+    return None 
+    
+async def get_town_by_trip():
+    res = await UserTown.find_one({'trip_number': None})
+    if not res:
+        raise HTTPException(status_code=status.HTTP_204_NO_CONTENT, detail='Email is not registry')
+    
+    return res
